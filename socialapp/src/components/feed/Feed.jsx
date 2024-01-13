859f53c02ba4bc9e5bee5da26d14b7467187e19e
swafey karanja <kswafey@gmail.com>
2024-01-13 18:41:16 +0300

    ...

--- socialapp/src/components/feed/Feed.jsx
+++ socialapp/src/components/feed/Feed.jsx
@@ -1,16 +1,23 @@
 import Share from "../share/Share.jsx"
 import Post from "../post/Post.jsx"
 import "./feed.css"
-import { Posts } from "../../Data.js";
+import { useEffect, useState } from "react"
 
-export default function feed() {
+
+export default function Feed() {
+  const [posts,setPosts] = useState([]);
+
+  useEffect(() =>{
+    console.log("xyz");
+  });
+
   return (
     <div className="feed">
       <div className="feedWrapper">
-        <Share/>
+        {/* <Share/>
         {Posts.map((p) => (
           <Post key={p.id} post={p}/>
-        ))}
+        ))} */}
       </div>
     </div>
   )
