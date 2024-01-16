0d07552dc8b969d7f5b2685ff84861e1c00bbc8e
swafey karanja <kswafey@gmail.com>
2024-01-16 16:38:25 +0300

    .....

--- noderestapi/routes/users.js
+++ noderestapi/routes/users.js
@@ -42,10 +42,15 @@ router.delete("/:id", async (req,res) =>{
 });
 
 //get a user
-router.get("/:id", async (req,res) =>{
+router.get("/", async (req,res) =>{
+    const userId = req.query.userId;
+    const username = req.query.username;
+
     try{
-        const user = await User.findById(req.params.id);
-        const {password, updatedAt, isAdmin, ...other} = user._doc
+        const user = userId 
+        ? await User.findById(userId)
+        : await User.findOne({ username : username });
+        const {password, updatedAt, isAdmin, ...other} = user._doc;
         res.status(200).json(other);
     }catch(err) {
         return res.status(500).json(err);
--- socialapp/src/pages/profile/Profile.jsx
+++ socialapp/src/pages/profile/Profile.jsx
@@ -1,13 +1,24 @@
 import "./profile.css"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Topbar from '../../components/Topbar/Topbar'
 import Sidebar from '../../components/sidebar/Sidebar'
 import Rightbar from '../../components/rightbar/Rightbar'
 import Feed from '../../components/feed/Feed'
+import axios from "axios"
 
 export default function Profile() {
 
+    const [user,setUser] = useState({});
     const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+
+    useEffect( () => {
+        const fetchUser = async () =>{
+          const res = await axios.get(`/users?username=Gon`);
+          setUser(res.data);
+        };
+        fetchUser();
+      },[]);
+
   return (
     <>
      <Topbar/>
@@ -20,8 +31,8 @@ export default function Profile() {
                         <img src={`${PF}posts/9.jpg`} alt="" className="profileUserImg" />
                     </div>
                     <div className="profileInfo">
-                        <h4 className="profileInfoName">Arthur Boyle</h4>
-                        <span className="profileInfoDesc">knight king</span>
+                        <h4 className="profileInfoName">{user.username}</h4>
+                        <span className="profileInfoDesc">{user.desc}</span>
                     </div>
                 </div>
                 <div className="profileRightBottom">
