bc032769edc922f431ec5634f0791da908205009
swafey karanja <kswafey@gmail.com>
2024-02-20 17:04:29 +0300

    back at it

--- socialapp/src/App.js
+++ socialapp/src/App.js
@@ -10,7 +10,7 @@ import {
 } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "./context/AuthContext";
-
+import Messenger from "./pages/messenger/Messenger";
 
 
 function App() {
@@ -20,9 +20,10 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path = "/" element={user ? <Home /> : <Register />}/>
+        <Route exact path = "/" element={user ? <Home /> : <Register />}/>
         <Route path = "/login" element={user ? <Navigate to="/" /> : <Login />}/>
         <Route path = "/register" element={user ? <Navigate to="/" /> : <Register />}/>
+        <Route path="/messenger" element={!user ? <Navigate to="/" /> : <Messenger />}/>
         <Route path = "/profile/:username" element={<Profile />}/>
       </Routes>
     </Router>
---
+++ socialapp/src/pages/messenger/Messenger.jsx
@@ -0,0 +1,12 @@
+import Topbar from "../../components/Topbar/Topbar"
+import "./messenger.css"
+
+export default function Messenger() {
+  return (
+    <>
+    <Topbar/>
+    <div>Messenger</div>
+    </>
+    
+  )
+}
